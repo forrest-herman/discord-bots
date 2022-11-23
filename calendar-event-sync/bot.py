25dd494b9fd06b217d1745a91a4acce434b83baf
forrest-herman <forrestherman13@gmail.com>
2022-11-23 15:47:19 -0500

    uncomment code

--- calendar-event-sync/bot.py
+++ calendar-event-sync/bot.py
@@ -90,13 +90,13 @@ async def on_ready():
 
     if (forrest_events):
         print(message)
-        # await channel.send(message)
+        await channel.send(message)
     else:
         print("No events for Forrest today")
 
     if (angele_events):
         print(message2)
-        # await channel.send(message2)
+        await channel.send(message2)
     else:
         print("No events for Angèle today")
 
